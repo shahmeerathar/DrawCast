b4bd96f64152903e9178a5af5b29f4aec57c491d
Shahmeer Athar <shahmeer.athar@gmail.com>
2025-01-13 23:00:58 -0500

    Draw buffer updates

--- main/DrawCast.c
+++ main/DrawCast.c
@@ -6,24 +6,66 @@
 #define UART_NUM UART_NUM_0
 #define BUF_SIZE 1024
 
-#define DISPLAY_HEIGHT 128
-#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 10
+#define DISPLAY_WIDTH 10
 
 char draw_buffer[DISPLAY_HEIGHT][DISPLAY_WIDTH];
 int cursor_x;
 int cursor_y;
 bool drawing;
 
-void handle_direction(char dir) {
-    if (dir == 'w') {
-        ESP_LOGI("DIRECTION", "Up");
-    } else if (dir == 'a') {
-        ESP_LOGI("DIRECTION", "Left");
-    } else if (dir == 's') {
-        ESP_LOGI("DIRECTION", "Down");
-    } else if (dir == 'd') {
-        ESP_LOGI("DIRECTION", "Right");
+inline int min(int a, int b) {
+    return a > b ? b : a;
+}
+
+inline int max(int a, int b) {
+    return a < b ? b : a;
+}
+
+void display_frame_buffer() {
+    char buffer_string[DISPLAY_HEIGHT * (DISPLAY_WIDTH + 1) + 1];
+    int idx = 0;
+    for (int y = 0; y < DISPLAY_HEIGHT; y++) {
+        for (int x = 0; x < DISPLAY_WIDTH; x++) {
+            buffer_string[idx] = draw_buffer[y][x] + 0x30;
+            idx++;
+        }
+        buffer_string[idx] = '\n';
+        idx++;
     }
+    buffer_string[idx] = '\0';
+    ESP_LOGI("FRAMEBUFFER", "\n%s", buffer_string);
+}
+
+void handle_input(char input) {
+    if (input == ' ') {
+        drawing = !drawing;
+        if (drawing) {
+            draw_buffer[cursor_y][cursor_x] = 1;
+        }
+        ESP_LOGI("DIRECTION", "Drawing flag set to: %d", drawing);
+        return;
+    }
+
+    if (input == 'w') {
+        ESP_LOGI("DIRECTION", "Up");
+        cursor_y = max(0, cursor_y - 1);
+    } else if (input == 'a') {
+        ESP_LOGI("DIRECTION", "Left");
+        cursor_x = max(0, cursor_x - 1);
+    } else if (input == 's') {
+        ESP_LOGI("DIRECTION", "Down");
+        cursor_y = min(DISPLAY_HEIGHT - 1, cursor_y + 1);
+    } else if (input == 'd') {
+        ESP_LOGI("DIRECTION", "Right");
+        cursor_x = min(DISPLAY_WIDTH - 1, cursor_x + 1);
+    }
+
+    ESP_LOGI("DIRECTION", "New coords: %d, %d", cursor_y, cursor_x);
+    
+    if (!drawing) { return; }
+    draw_buffer[cursor_y][cursor_x] = 1;
+    display_frame_buffer();
 }
 
 void app_main(void)
@@ -52,7 +94,7 @@ void app_main(void)
     while (1) {
         int len = uart_read_bytes(UART_NUM, data, BUF_SIZE, 20 / portTICK_PERIOD_MS);
         if (len == 1) {
-            handle_direction(data[0]);
+            handle_input(data[0]);
         }
     }
 }
